d0e0726e2a8f6e4fe8e9737022c89d2c16c371f3
szymon <vatrasar@gmail.com>
2019-12-06 01:18:28 +0100

    add items to choice box

--- src/main/java/org/example/PropertiesController.java
+++ src/main/java/org/example/PropertiesController.java
@@ -1,13 +1,17 @@
 package org.example;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 import java.util.logging.Logger;
 
 public class PropertiesController {
+
+    ChoiceBox choiceBox;
     @FXML
     public void startTest()
     {
         Logger.getGlobal().info("Test starts");
+
     }
 }
